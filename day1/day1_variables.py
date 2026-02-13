3b45638da7be11fe24a03e547be346671c4fab3c
coderx-shamsher <shamsher000789@gmail.com>
2026-02-13 22:04:12 +0530

    day one basic variable python code

---
+++ day1/day1_variables.py
@@ -0,0 +1,30 @@
+# note make sure to read the readme file to understand the purpose of this code this is my code 
+# python variables is containers that store values. python is not "statically typed", we don't need to declare variables before using them or declare their type.
+# ham variable mein koi bhi value store kr sakte hain, aur ham usko use kr sakte hain kabhi bhi code main ,
+
+'''
+    Syntax of creating a variable in python :
+    variable_name = value   
+    
+    no need to declare the type of variable in python, python automatically detects the type of variable based on the value assigned to it.
+
+'''
+variable = 100 
+print()
+print("first value in variable is:")
+print(variable)
+
+
+#  types of data in python / Data Types in python 
+# Variables and there types with type () function 
+# python Data Types are the classification or categorization of data items..
+#  we see there types using type() function
+
+st = "this is value of string "
+print()
+print(st)
+print(type(st))
+print()
+print(type(variable))
+
+# ham kisi bhi variable ka type print kr sakte hain using the type() function, aur ham usko print kr sakte hain

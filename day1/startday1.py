b86f4de64c10a224e3c069d3d16e2a9e4a7f486f
coderx-shamsher <shamsher000789@gmail.com>
2026-02-13 22:05:41 +0530

    the starting of python print function

---
+++ day1/startday1.py
@@ -0,0 +1,15 @@
+#          // day one in python \\ 
+
+# 1. print statement / print function(method)
+# print() is a built-in function in Python that is used to display output on the console. It can take multiple arguments and will print them to the console, separated by a space by default.
+print()
+print("this is first day in python " )
+
+# => how to print of run the python code just open the terminal and type the python and then 
+# python filename.py 
+
+# ham print() function mein multiple values ko print kar sakte hain, aur ham unko comma se separate krte hain  
+print("this is hello","python",111,222)
+
+# 1) print () mein ham es trah say print kr sakte hain values koi or now ham dekhenge concept of the variables in python 
+

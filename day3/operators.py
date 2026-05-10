17c6d4a5a6ee8756047a68c47393abe02a476cb7
coderx-shamsher <shamsher000789@gmail.com>
2026-05-10 10:08:53 +0200

    operators in python (arithmetic Opr)

---
+++ day3/operators.py
@@ -0,0 +1,53 @@
+#In Python programming, Operators in general are used to perform operations on values and variables.
+#Operators: Special symbols like -, + , * , /, etc.
+#Operands: Value on which the operator is applied.
+
+
+'''
+1) Arithmetic Operators 
+Arithmetic operators are used to perform basic mathematical operations like addition, subtraction, multiplication and division.
+
+'''
+
+
+#  addition ( + ) 
+
+a = 200 
+b =  300 
+print(a,"+",b,"is =",a+b)
+
+# subtraction ( - )
+print(b,"-",a,"is =",b-a)
+
+# multiplication ( * )
+print(a,"*",b,"is =",a*b)
+
+# Division ( / )
+num1 = 240
+num2 = 8
+print(num1,"/",num2,"is =",num1/num2)
+
+
+# floor Division ( // )
+print(num1,"//",num2,"is =",num1//num2)
+
+" -> Note -> In Python, the division operator (/) returns a floating-point result, while floor division (//) returns an integer result. "
+
+
+# modulus ( % )
+print(num1,"%",num2,"is =",num1%num2)
+
+# exponentiation ( ** )
+n1 = 2
+n2 = 4 
+print(n1,"**",n2,"is = ", n1**n2)
+# 2 * 2 * 2 * 2   2 ** 4 means 2 ko 4 bari multiple kro kaise ? -> 2 * 2 = 4 * 2 = 8 * 2 = 16
+# that's how exponentiation works 
+  
+print()
+print(n1*n2)
+print(2**3)
+
+
+
+

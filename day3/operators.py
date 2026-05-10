f1f4cdf057ad7467f2c92e2a9a885e2ef1f5ff1e
coderx-shamsher <shamsher000789@gmail.com>
2026-05-10 11:07:22 +0200

    + comparison and assignment Opr

--- day3/operators.py
+++ day3/operators.py
@@ -48,6 +48,65 @@ print()
 print(n1*n2)
 print(2**3)
 
+print()
+"2) Comparison Operators -> Comparison(or Relational) operators compares values. It either returns True or False according to the condition."
 
+# == equal to opr 
+var1 = 20
+var2 = 22
+print(var1,"==",var2," ->",var1 == var2)
+print()
+
+# != not equal to 
+print(var1,"!=",var2," ->",var1 != var2)
+print() 
+
+# > greater then 
+print(var1,">",var2," ->",var1 > var2)
+# print(var1 > var2)
+print()
+
+# < less then 
+print(var1,"<",var2," ->",var1 < var2)
+# print(var1 < var2) 
+print()
+
+# >= greater than equal to  yeh dono conditions ko check kreha > and = to 
+print(var1,">=",var2," ->",var1 >= var2)
+# print(var1 >= var2)
+print()
+
+# <= less than equal to 
+print(var1,"<=",var2," ->",var1 <= var2)
+# print(var1 <= var2) 
+print()
+
+# Assignment OPR
+"Assignment operators are used to assign values to the variables. This operator is used to assign the value of the right side of the expression to the left side operand. Example:"
+
+# = assignment opr
+a = 200
+print("this is the assignment opr ")
+print()
+
+# Assignment operators with arithmetic assignments 
+
+# 1) a = a + value/number/variable
+a = 369 
+b = 1 
+
+# variable 
+print("before Assignment => ",a)
+a = a + b 
+print("After Assignment => ",a)
+
+print()
+# 2) a += value/number/variable
+print("before -> ",a,"\n")
+a += b 
+print("after -> ",a)
+
+
+"or ham same he subraction, or other arithmetic operators use kr skate hain. example -> alll he bitwise oprs etc... "
 
 

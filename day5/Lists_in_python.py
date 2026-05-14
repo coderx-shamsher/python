b028286ef2246129e26799fbf609ffdbf0c43e3e
coderx-shamsher <shamsher000789@gmail.com>
2026-05-14 19:06:47 +0200

    list in python

---
+++ day5/Lists_in_python.py
@@ -0,0 +1,26 @@
+## Lists in python.. 
+''' 
+Lists in python is just like an array , we used in c++ or javascript etc.. 
+
+'''
+
+# first empty list 
+list = []
+print(list)
+
+# list with elements same type values  
+list = [10,20,30]
+print("All the elements ->",list)
+print()
+
+# getting elements with indexing same method
+print(list[0])
+print(list[-1])
+
+# list with multple type of value /elements 
+list1 = ["hello",100,30.3]
+print("Multiple type list -> ",list1)
+
+# or ham same indexing apply kr sakte hain....  
+
+

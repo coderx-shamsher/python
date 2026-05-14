5752fc4e35eae8f812e0e98c580ab5e86d7bef51
coderx-shamsher <shamsher000789@gmail.com>
2026-05-14 17:35:38 +0200

    strings in python

---
+++ day4/strings.py
@@ -0,0 +1,53 @@
+# what is strings in python 
+"Strings in python is collection of one or more characters put in a single , double and triple quotes"
+
+# single quote string 
+string = 'H'
+print("single quote string => ",string)
+
+# ham characters or words koi single quotes mein store kr sakte hain.. 
+
+# double quotes 
+string = "hello"
+print("duoble quotes => ",string)
+
+
+# triple quotes 
+triple = '''
+             this string type for more then one lines in code 
+             or also used in commenting lines 
+             in python we can use # and '' and "" also triple quotes to commenting lines 
+'''
+
+print()
+print(triple)
+print()
+
+## indexing concept in strings to access characters 
+
+# getting first character 
+# also it is positive indexing 
+# indexing in python start from 0 
+print(string[0])
+print(string[1])
+print(string[2])
+
+# how to get the length of the string 
+# using the len() function/method 
+print(len(string))
+# ham kisi bhi variable ki length pta kr sakte hain.. 
+
+# ager length 5 hai lets say to meri indexing 4 tak hogi keoki 0 first element store krta hai .... 
+## last element get krna.. using positive indexing 
+print(string[4])
+
+# nagitive indexing .... 
+# nagitive indexing start from -1 
+print() 
+print(string[-1])
+print()
+
+# Note -> make sure it space bhi as character count hoti hai.. 
+
+
+

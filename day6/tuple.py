b0a4a97ab3f03e9c6f5d78c3a8652426cc066323
coderx-shamsher <shamsher000789@gmail.com>
2026-05-14 19:07:19 +0200

    tuples in python

---
+++ day6/tuple.py
@@ -0,0 +1,19 @@
+## tuple in python  ..... 
+"just line list tuple also is ordered collection of elements, or can cantains multi-typed values , the difference is tuple cannot be modified once its created.. "
+
+t =  ()
+print('this is empty tuple ->',t) 
+
+
+tt = ("arch","linux")
+print()
+
+# accessing element using indexing method in tuple...
+print(tt[0])
+print(tt[-1])
+
+## multiple values in tuple 
+t_t  = ("coder",1122,490.67)
+print()
+print(t_t)
+

54aa2cff2029eb91b89aa6933e6395b0756f139b
coderx-shamsher <shamsher000789@gmail.com>
2026-05-24 17:14:00 +0200

    boolean calculations

---
+++ day3/values_with_operators.py
@@ -0,0 +1,17 @@
+#### --> 
+# True and False are actually 1 and 0 but with different keywords 
+print()  
+print("sum of boolean values ( true + true) => ", True + True)
+
+## why 2 because true is 1 and false is 0 
+print("sum of boolean values ( false + true) => ", False + True)
+
+print()
+
+print("sum of boolean values ( 20 + true) => ", 20 + True)
+
+print()
+
+print("sum of boolean values ( 24 * true) => ", 24 + True)
+
+## that the boolean calculations 

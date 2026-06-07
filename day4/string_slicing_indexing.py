7a431d692c14dfaba38c2ed8397ae0ddb10a2e59
coderx-shamsher <shamsher000789@gmail.com>
2026-06-07 19:19:15 +0200

    update the slicing code..

--- day4/string_slicing_indexing.py
+++ day4/string_slicing_indexing.py
@@ -122,6 +122,24 @@ print(after)
 
 
 print()
+
+text101 = "this linux is the most powerfull os in the world"
+
+part1,part2,part3 = text101.partition("most")
+print(part1,type(part1))
+print(part2,type(part2))
+print(part3,type(part3))
+
+### simple way jis value ki slice krna hai use partition method mein pass kro or use ek middle position variable mein store kro or that it its easy to get this value , bs jis value ko splice krna hai vo mid variable mein store hoti hai or baki divide ho jate hain 3 parts mein ... 2 vala part hamara result hota hai... 
+
+# print(part2)
+# print(part3)
+subpart1  = part1.partition("linux")
+print(subpart1,"\n",type(subpart1))
+
+## use indexing to get element from tuple now... 
+print(subpart1[0])
+
 # Dynamic extraction
 text = "Order ID: 45872 | Status: Success"
 
@@ -154,3 +172,31 @@ print(result)
 
 result = result.split(" of")[0]
 print(result)
+print()
+## this method split() break down 
+## jo bhi value split function main pass hoti hai us jagah string cut hoti hai 
+
+test001  = "linux have ubantu arch fedora red-hat debain"
+
+## using he split() method 
+### this is step 1 
+splited_value = test001.split(" ")
+print(splited_value)
+print()
+splited_value = test001.split("ubantu ")
+print(splited_value)
+
+## hamne string ko ek list mein break kr liya... 
+## keo k split() method ek list he return krega hamesa to jis value ko pass kroge to us jagah te including your passed string value cut ho jayegi jaise maine "ubantu " pass kiya mean "ubantu " koi include krte hoye meri string break hogi
+print()
+print(splited_value[0])
+print(splited_value[1]) 
+print()
+## now muje arch chaahie to let do same method 
+result2 = splited_value[1].split("fedora")
+result2 = splited_value[1].split("fedora")[0]
+print("MY sliced value is =>",result2)
+
+# you can do below method to do the same thing..
+# print(result2[0])
+
